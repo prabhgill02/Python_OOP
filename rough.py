de5a8288b1dec75b2e9c93170d2760eb3abbb7ca
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2025-12-24 13:25:59 -0500

    Basic OOps

--- rough.py
+++ rough.py
@@ -1,6 +1,9 @@
 from oops_proj import chatbook
 
 user1 = chatbook()
+# print(chatbook._chatbook__user_id) -- this is the way to access hidden variable or the one mentioned below
+# but the best practice is using the concept of getter and setter usingg decorator either @classmethod
+#  or @staticmethod decorators
 print(user1.id)
 # print(user1._chatbook__name) # to access hidden variable outside we have to add class name as well
 
@@ -12,13 +15,11 @@ print(user2.id)
 user3 = chatbook()
 print(user3.id)
 
-
-
-
-# --- getter and setter
-# print(user1.get_name())
-# user1.set_name("Agent X")
-# print(user1.get_name())
+print(user3._chatbook__name)  #---- This is a way to access the hidden variable outside 
+# --- getter and setter # -- using getter and setter, we can directly access inside the method using self
+print(user1.get_name())
+user1.set_name("Agent X")
+print(user1.get_name())
 
 
 
